d714a073342aecb8a8db249adb28bcf8b1dfcdef
Roni Castro <roni.c.castro@gmail.com>
2023-03-28 23:21:25 -0300

    feat: create seeds and models

---
+++ aula05-tdd-project-pt02/src/entities/carCategory.js
@@ -0,0 +1,11 @@
+const Base = require('./base');
+
+class CarCategory extends Base {
+  constructor({id, name, carIds, pricePerDay}) {
+    super({id, name});
+    this.carIds = carIds;
+    this.pricePerDay = pricePerDay;
+  }
+}
+
+module.exports = CarCategory;
