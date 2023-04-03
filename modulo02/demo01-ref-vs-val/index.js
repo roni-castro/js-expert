2e386523f0c9cc860f6999614b2d55b0d761bfbd
Roni Castro <roni.c.castro@gmail.com>
2023-04-02 23:18:23 -0300

    feat: demo type by value or by reference

---
+++ modulo02/demo01-ref-vs-val/index.js
@@ -0,0 +1,23 @@
+const {deepStrictEqual} = require('assert');
+
+let counter = 0;
+let counter2 = counter;
+counter2++;
+// A: counter = 0, counter2 = 1
+// valor primitivo é copiado por valor
+
+deepStrictEqual(counter, 0);
+deepStrictEqual(counter2, 1);
+
+const item = {counter: 0};
+const item2 = item;
+item2.counter++;
+// A: item.counter = 1
+//    item.counter2 = 1
+// objeto é copiado por e aponta para mesma referência
+
+deepStrictEqual(item, {counter: 1});
+deepStrictEqual(item2, {counter: 1});
+item2.counter++;
+deepStrictEqual(item, {counter: 2});
+deepStrictEqual(item2, {counter: 2});
