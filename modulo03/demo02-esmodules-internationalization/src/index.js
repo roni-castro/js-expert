88005265234b6f14fda5455eab9cd3448993f9d1
Roni Castro <roni.c.castro@gmail.com>
2023-04-03 22:03:20 -0300

    refactor: add controller and to handle terminal logic

--- modulo03/demo02-esmodules-internationalization/src/index.js
+++ modulo03/demo02-esmodules-internationalization/src/index.js
@@ -1,36 +1,25 @@
-import DraftLog from 'draftlog';
-import chalk from 'chalk';
-import chalkTable from 'chalk-table';
-import readLine from 'readline';
 import database from './../database.json' assert {type: 'json'};
-import Person from './person.js';
+import TerminalController from './terminalController.js';
 
 const DEFAULT_LANGUAGE = 'pt-br';
+const TERMINAL_CLOSE_COMMAND = ':q';
 
-DraftLog(console).addLineListener(process.stdin);
+const terminalController = new TerminalController();
+terminalController.initializeTerminal(database, DEFAULT_LANGUAGE);
 
-const options = {
-  leftPad: 2,
-  columns: [
-    {field: 'id', name: chalk.cyan('ID')},
-    {field: 'vehicles', name: chalk.magenta('Vehicles')},
-    {field: 'kmTraveled', name: chalk.cyan('Km Traveled')},
-    {field: 'from', name: chalk.magenta('From')},
-    {field: 'to', name: chalk.cyan('To')}
-  ]
-};
+async function mainLoop() {
+  try {
+    const answer = await terminalController.question('question??');
+    if (answer === TERMINAL_CLOSE_COMMAND) {
+      terminalController.closeTerminal();
+      console.log('process finished!');
+      return;
+    }
+    mainLoop();
+  } catch (error) {
+    console.log(error);
+    mainLoop();
+  }
+}
 
-const table = chalkTable(
-  options,
-  database.map((data) => new Person(data).formatted(DEFAULT_LANGUAGE))
-);
-const print = console.draft(table);
-
-const terminal = readLine.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-terminal.question('pergunta?', (msg) => {
-  console.log('msg', msg.toString());
-});
+await mainLoop();
---
+++ modulo03/demo02-esmodules-internationalization/src/terminalController.js
@@ -0,0 +1,58 @@
+import readLine from 'readline';
+import chalk from 'chalk';
+import chalkTable from 'chalk-table';
+import DraftLog from 'draftlog';
+import Person from './person.js';
+
+export default class TerminalController {
+  constructor() {
+    this.readLine = readLine;
+    this.chalkTable = chalkTable;
+    this.print = {};
+    this.data = [];
+    this.terminal = {};
+  }
+
+  initializeTerminal(database, language) {
+    DraftLog(console).addLineListener(process.stdin);
+    this.terminal = this.readLine.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    this.initializeTable(database, language);
+  }
+
+  initializeTable(database, language) {
+    const tableData = database.map((data) =>
+      new Person(data).formatted(language)
+    );
+    const table = this.chalkTable(this.getTableOptions(), tableData);
+    this.print = console.draft(table);
+    this.data = tableData;
+  }
+
+  question(question = '') {
+    return new Promise((resolve) => {
+      this.terminal.question(question, resolve);
+    });
+  }
+
+  closeTerminal() {
+    this.terminal.close();
+  }
+
+  getTableOptions() {
+    const options = {
+      leftPad: 2,
+      columns: [
+        {field: 'id', name: chalk.cyan('ID')},
+        {field: 'vehicles', name: chalk.magenta('Vehicles')},
+        {field: 'kmTraveled', name: chalk.cyan('Km Traveled')},
+        {field: 'from', name: chalk.magenta('From')},
+        {field: 'to', name: chalk.cyan('To')}
+      ]
+    };
+    return options;
+  }
+}
