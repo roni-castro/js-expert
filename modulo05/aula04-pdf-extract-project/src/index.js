84fd7e49b5afc11d99eab23e6d7db6e42349e261
Roni Castro <roni.c.castro@gmail.com>
2023-04-09 20:27:44 -0300

    feat: create facade to handle getting people from pdf

--- modulo05/aula04-pdf-extract-project/src/index.js
+++ modulo05/aula04-pdf-extract-project/src/index.js
@@ -1,11 +1,13 @@
 const {readFile} = require('fs/promises');
 const path = require('path');
 const pdfParse = require('pdf-parse');
+const TextProcessorFacade = require('./textProcessorFacede');
 
 (async function loadPDFFile() {
   const dataBuffer = await readFile(
     path.join(__dirname, '../files/contrato.pdf')
   );
   const pdfResult = await pdfParse(dataBuffer);
-  console.log(pdfResult.text);
+  const people = new TextProcessorFacade(pdfResult.text).getPeopleFromPDF();
+  console.log(people);
 })();
---
+++ modulo05/aula04-pdf-extract-project/src/textProcessorFacede.js
@@ -0,0 +1,18 @@
+const TextProcessorFluentAPI = require('./textProcessorFluentAPI');
+
+class TextProcessorFacade {
+  #textProcessorFluentAPI;
+  constructor(text) {
+    this.#textProcessorFluentAPI = new TextProcessorFluentAPI(text);
+  }
+
+  getPeopleFromPDF() {
+    return this.#textProcessorFluentAPI
+      .extractPeopleData()
+      .divideTextInColumns()
+      .removeEmptyChars()
+      .mapPerson()
+      .build();
+  }
+}
+module.exports = TextProcessorFacade;
