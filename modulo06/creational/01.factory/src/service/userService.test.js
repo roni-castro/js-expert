773bc01662c9b01dff753d058e2b575dc5f8fbca
Roni Castro <roni.c.castro@gmail.com>
2023-04-22 16:42:19 -0300

    feat: create user service

---
+++ modulo06/creational/01.factory/src/service/userService.test.js
@@ -0,0 +1,35 @@
+const sinon = require('sinon');
+const {describe, it} = require('mocha');
+const {expect} = require('chai');
+
+const UserService = require('./userService');
+const UserRepository = require('../repository/userRepository');
+
+describe('UserService', () => {
+  let sandbox;
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+  });
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('#find', () => {
+    it('should call userRepository.find with query and return uppercase usernames', async () => {
+      const query = {name: 'John Doe'};
+      const expectedResult = ['USER1', 'USER2'];
+      // Create a stub for the userRepository object
+      const userRepository = {
+        find: sandbox.stub().resolves(['user1', 'user2'])
+      };
+      const userService = new UserService({userRepository});
+
+      const result = await userService.find(query);
+
+      expect(result).to.deep.equal(expectedResult);
+      // Assert that userRepository.find was called with the query
+      expect(userRepository.find.calledOnce).to.be.true;
+      expect(userRepository.find.calledWith(query)).to.be.true;
+    });
+  });
+});
