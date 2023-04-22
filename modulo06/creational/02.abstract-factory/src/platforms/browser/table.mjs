8cd606206728fc3f926d40cf9c61b39d08e4771c
Roni Castro <roni.c.castro@gmail.com>
2023-04-22 18:19:56 -0300

    feat: create browser table component

--- modulo06/creational/02.abstract-factory/src/platforms/browser/table.mjs
+++ modulo06/creational/02.abstract-factory/src/platforms/browser/table.mjs
@@ -2,6 +2,36 @@ import TableComponent from '../../shared/base/tableComponent.mjs';
 
 export default class TableBrowserComponent extends TableComponent {
   render(data) {
-    window.alert('table browser');
+    const template = this.prepareData(data);
+    document.body.insertAdjacentHTML('afterbegin', template);
+  }
+
+  prepareData(data) {
+    const [firstRow] = data;
+    const tableHeaders = Object.keys(firstRow).map(
+      (header) => `<th scope="col">${header}</th>`
+    );
+
+    const joinLists = (list) => list.join('');
+
+    const tableBodyValues = data
+      .map((item) => Object.values(item))
+      .map((item) => item.map((value) => `<td>${value}</td>`))
+      .map((tds) => `<tr>${joinLists(tds)}</tr>`);
+
+    const template = `
+      <table class="table">
+        <thead class="thead-dark">
+        <tr>
+          ${joinLists(tableHeaders)}
+        </tr>
+        </thead>
+        <tbody>
+          ${joinLists(tableBodyValues)}
+        </tbody>
+      </table>
+    `;
+
+    return template;
   }
 }
