998ea3a7377f692a54ed7ce787bf7f4f22979398
Roni Castro <roni.c.castro@gmail.com>
2023-04-22 17:05:51 -0300

    feat: create user factory

---
+++ modulo06/creational/01.factory/src/factory/userFactory.js
@@ -0,0 +1,16 @@
+const UserRepository = require('../repository/userRepository');
+const UserService = require('../service/userService');
+const Database = require('../utils/database');
+
+class UserFactory {
+  static async createInstance() {
+    const database = new Database('mongo://localhost');
+    const dbConnection = await database.connect();
+    const userRepository = new UserRepository({dbConnection});
+    const userService = new UserService({userRepository});
+
+    return userService;
+  }
+}
+
+module.exports = UserFactory;
---
+++ modulo06/creational/01.factory/src/factory/userFactory.test.js
@@ -0,0 +1,30 @@
+const {describe, it} = require('mocha');
+const {expect} = require('chai');
+const rewiremock = require('rewiremock/node');
+
+const dbData = [{name: 'user1'}, {name: 'user2'}];
+class MockDatabase {
+  connect = () => this;
+  find = async (_query) => dbData;
+}
+
+describe('UserFactory', () => {
+  beforeEach(() => {
+    rewiremock.enable();
+  });
+  afterEach(() => {
+    rewiremock.disable();
+  });
+
+  it('should create an instance of UserService with a UserRepository and a dbConnection', async () => {
+    rewiremock(() => require('../utils/database')).with(MockDatabase);
+    const query = {name: 'anyName'};
+    const expected = [{name: 'USER1'}, {name: 'USER2'}];
+    const UserFactory = rewiremock.proxy(() => require('./userFactory'));
+
+    const userService = await UserFactory.createInstance();
+    const result = await userService.find(query);
+
+    expect(result).to.be.deep.equal(expected);
+  });
+});
