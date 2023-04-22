02f73ecb596b771e5c9a4a84361704175e18dbb5
Roni Castro <roni.c.castro@gmail.com>
2023-04-22 16:31:18 -0300

    feat: create user repository

---
+++ modulo06/creational/01.factory/src/repository/userRepository.test.js
@@ -0,0 +1,34 @@
+const sinon = require('sinon');
+const {describe, it} = require('mocha');
+const {expect} = require('chai');
+
+const UserRepository = require('./userRepository');
+
+describe('UserRepository', () => {
+  let sandbox;
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+  });
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('#find', () => {
+    it('should call dbConnection.find with query', async () => {
+      const query = {name: 'John Doe'};
+      const expectedResult = ['user1', 'user2'];
+      // Create a stub for the dbConnection object
+      const dbConnection = {
+        find: sandbox.stub().resolves(expectedResult)
+      };
+      const userRepository = new UserRepository({dbConnection});
+
+      const result = await userRepository.find(query);
+
+      expect(result).to.equal(expectedResult);
+      // Assert that dbConnection.find was called with the query
+      expect(dbConnection.find.calledOnce).to.be.true;
+      expect(dbConnection.find.calledWith(query)).to.be.true;
+    });
+  });
+});
---
+++ modulo06/creational/01.factory/src/utils/database.js
@@ -0,0 +1,23 @@
+class Database {
+  constructor(connectionString) {
+    this.connectionString = connectionString;
+  }
+
+  async #sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
+
+  async connect() {
+    await this.#sleep(200);
+    return this;
+  }
+
+  async find(_query) {
+    await this.#sleep(200);
+    return [{name: 'Roni Castro'}];
+  }
+}
+
+module.exports = Database;
