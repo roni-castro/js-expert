2db8178635db45e4c0bd909519c4f745550daab1
Roni Castro <roni.c.castro@gmail.com>
2024-02-25 23:04:41 -0300

    chore: create seeds and mocks

--- modulo01/aula05-tdd-project-pt02/src/entities/carCategory.js
+++ modulo01/aula05-tdd-project-pt02/src/entities/carCategory.js
@@ -1,9 +1,8 @@
-'use strict';
 const Base = require('./base');
 
 class CarCategory extends Base {
-  constructor({id, name, carIds, pricePerDay}) {
-    super({id, name});
+  constructor({ id, name, carIds, pricePerDay }) {
+    super({ id, name });
     this.carIds = carIds;
     this.pricePerDay = pricePerDay;
   }
