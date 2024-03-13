21b51a020911f3817369fb418e90f96019b46c6a
Roni Castro <roni.c.castro@gmail.com>
2024-03-12 23:34:05 -0300

    feat: improve set and weak set examples

--- modulo04/aula04-set-weakset/index.js
+++ modulo04/aula04-set-weakset/index.js
@@ -6,39 +6,42 @@ const set = new Set();
 const arr1 = ['0', '1', '2'];
 const arr2 = ['2', '0', '3'];
 const arr1arr2 = arr1.concat(arr2).sort();
-assert.deepStrictEqual(arr1arr2, ['0', '0', '1', '2', '2', '3']);
+assert.deepStrictEqual(arr1arr2, ['0', '0', '1', '2', '2', '3'])
+arr1.forEach((v) => set.add(v));
+arr2.forEach((v) => set.add(v));
+assert.deepStrictEqual(Array.from(set), ['0', '1', '2', '3'])
 
-arr1.map((v) => set.add(v));
-arr2.map((v) => set.add(v));
-assert.deepStrictEqual(Array.from(set), ['0', '1', '2', '3']);
-
-const setWithConstructor = new Set([...arr1, ...arr2]);
-assert.deepStrictEqual(Array.from(setWithConstructor), ['0', '1', '2', '3']);
+const setWithConstructor1 = new Set(arr1)
+assert.deepStrictEqual(Array.from(setWithConstructor1), ['0', '1', '2'])
+const setWithConstructor2 = new Set([...arr1, ...arr2])
+assert.deepStrictEqual(Array.from(setWithConstructor2), ['0', '1', '2', '3'])
 
 // - keys
-assert.deepStrictEqual([...set.keys()], ['0', '1', '2', '3']); // compatibility with Map
+assert.deepStrictEqual([...setWithConstructor2.keys()], ['0', '1', '2', '3']);  // compatibility with Map
 // - values
-assert.deepStrictEqual([...set.values()], ['0', '1', '2', '3']);
+assert.deepStrictEqual([...setWithConstructor2.values()], ['0', '1', '2', '3']);  // compatibility with Map
 // - entries
-assert.deepStrictEqual(
-  [...set.entries()],
-  [
-    ['0', '0'],
-    ['1', '1'],
-    ['2', '2'],
-    ['3', '3']
-  ]
-);
+assert.deepStrictEqual([...setWithConstructor2.entries()], [
+  ['0', '0'],
+  ['1', '1'],
+  ['2', '2'],
+  ['3', '3']
+])
 // - has
-assert.deepStrictEqual(set.has('0'), true);
-assert.deepStrictEqual(set.has('notAKey'), false);
+assert.deepStrictEqual(setWithConstructor2.has('0'), true);
+assert.deepStrictEqual(setWithConstructor2.has('notAKey'), false);
+// - delete
+assert.deepStrictEqual(setWithConstructor2.delete('0'), true);
+assert.deepStrictEqual(setWithConstructor2.delete('notAKey'), false);
 
 const ids01 = new Set(['0', '1', '2']);
 const ids02 = new Set(['3', '0', '4']);
-const intersection = new Set([...ids01].filter((id1) => ids02.has(id1)));
+const intersection = new Set([...ids01].filter(id1 => ids02.has(id1)))
 assert.deepStrictEqual([...intersection], ['0']);
-const difference = new Set([...ids01].filter((id1) => !ids02.has(id1)));
-assert.deepStrictEqual([...difference], ['1', '2']);
+const difference12 = new Set([...ids01].filter(id1 => !ids02.has(id1)));
+assert.deepStrictEqual([...difference12], ['1', '2'])
+const difference21 = new Set([...ids02].filter(id2 => !ids01.has(id2)));
+assert.deepStrictEqual([...difference21], ['3', '4']);
 
 // WeakSet
 const weakSet = new WeakSet();
@@ -50,8 +53,8 @@ weakSet.has(hero);
 
 // Feature--------------Set--------------WeakSet
 // ---------------------------------------------
-// Value type-----------Any--------------Object-only
-// Value reference--------Strong-----------Weak
+// Value type-----------Any--------------Object-only (reference)
+// Value reference------Strong-----------Weak
 // Garbage collection*--No---------------Yes
 // Iteration order------Insertion--------N/A
 
