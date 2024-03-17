71cf88de9e13d60ee4dd2e52a93ad4a8f4969bc5
Roni Castro <roni.c.castro@gmail.com>
2024-03-17 14:28:31 -0300

    refactor: update text processor test

--- modulo05/aula02-pdf-extract-project/src/textProcessorFluentAPI.js
+++ modulo05/aula02-pdf-extract-project/src/textProcessorFluentAPI.js
@@ -1,25 +1,24 @@
 'use strict'
 class TextProcessorFluentAPI {
-  #content;
+  #content
   constructor(content) {
-    this.#content = content;
+    this.#content = content
   }
 
   build() {
-    return this.#content;
+    return this.#content
   }
 
   extractPeopleData() {
-    // Get the people data that comes after the Contratante or Contratada (insensitive)
+        // Get the people data that comes after the Contratante or Contratada (insensitive)
     // (?<=[contratante|contratada]\:\s) find Contratante or Contratada pattern and ignores it
     // (?!\s) ignores the Contratante or Contratada at the end of the file, that has
     // another space instead of the person data.
     // (.*\n.*?)$ gets the person data we want including those with break lines, until
     // the end of line, as few as possible
-    const peopleDataRegex =
-      /(?<=[contratante|contratada]\:\s)(?!\s)(.*\n.*?)$/gim;
-    this.#content = this.#content.match(peopleDataRegex);
-    return this;
+    const peopleDataRegex = /(?<=[contratante|contratada]\:\s)(?!\s)(.*\n.*?)$/gim
+    this.#content = this.#content.match(peopleDataRegex)
+    return this
   }
 }
 
--- modulo05/aula02-pdf-extract-project/src/textProcessorFluentAPI.test.js
+++ modulo05/aula02-pdf-extract-project/src/textProcessorFluentAPI.test.js
@@ -22,10 +22,6 @@ describe('TextProcessorFluentAPI Suit tests', () => {
         'domiciliada a Rua dos bobos, zero, bairro Alphaville, São Paulo. '
       ].join('\n'),
       [
-        'Arya Robbin, belga, casado, CPF 884.112.200-52, residente e ',
-        'domiciliada a Av. paulista, 1400, bairro Consolação, São Paulo. '
-      ].join('\n'),
-      [
         'Júlia Menezes, brasileira, solteira, CPF 297.947.800-81, residente e ',
         'domiciliada a Av. dos Estados, 99, bairro Jardins, São Paulo. '
       ].join('\n')
